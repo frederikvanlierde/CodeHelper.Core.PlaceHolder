209e63006009a59ffdc62fc05c5e01dd467e969c
frede <frede@FREDERIK-ACER>
2022-08-04 20:00:27 +0200

    Add project files.

---
+++ CodeHelper.Core.PlaceHolder/PlaceHolderHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+namespace CodeHelper.Core.PlaceHolder
+{
+    public static class PlaceHolderHelper
+    {
+        /// <summary>
+        /// Replace the placeholders in a text with the values of the object
+        /// </summary>
+        /// <param name="text">String: the text containing the placeholders</param>
+        /// <param name="MyObject">object: the object containing the values to replace the placeholders</param>
+        /// <returns>string: the original text with the placeholders replaced with the real values.</returns>
+        public static string Replace(string text, object MyObject)
+        {
+            if (MyObject != null)
+            {
+                List<PropertyInfo> placeholders = MyObject.GetType().GetProperties().ToList().FindAll(p => p.GetCustomAttribute(typeof(PlaceholderAttribute)) != null);
+                if (placeholders != null && placeholders.Count != 0)
+                {
+                    foreach (PropertyInfo placeholder in placeholders)
+                        text = text.Replace(((PlaceholderAttribute)placeholder.GetCustomAttribute(typeof(PlaceholderAttribute))).Text, Convert.ToString(placeholder.GetValue(MyObject)));
+                }
+            }
+            return text;
+        }
+    }
+}
---
+++ CodeHelper.Core.PlaceHolder/PlaceholderAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeHelper.Core.PlaceHolder
+{
+    [System.AttributeUsage(System.AttributeTargets.Field |
+                       System.AttributeTargets.Property)
+    ]
+    public class PlaceholderAttribute : System.Attribute
+    {
+        #region Properties        
+        public string Text { get; set; } = "";
+        #endregion
+
+        #region Constructors
+        public PlaceholderAttribute(string text)
+        {
+            this.Text = text;
+        }
+        #endregion
+    }
+}
